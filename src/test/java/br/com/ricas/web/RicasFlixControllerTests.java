3d90e0b078e5324a48a48f1e7527ea86f40ee201
Ricardo Mello <ricardohsmello@gmail.com>
2026-08-15 16:49:12 -0300

    Adding project   files

---
+++ src/test/java/br/com/ricas/web/RicasFlixControllerTests.java
@@ -0,0 +1,33 @@
+package br.com.ricas.web;
+
+import br.com.ricas.movie.MovieSearchResult;
+import br.com.ricas.movie.MovieSearchService;
+import br.com.ricas.web.dto.MovieSearchRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class RicasFlixControllerTests {
+
+	@Test
+	void acceptsRequestContainingOnlyQuery() throws Exception {
+		MovieSearchService service = new MovieSearchService(null, null, null) {
+			@Override
+			public List<MovieSearchResult> searchMovies(MovieSearchRequest request) {
+				return List.of();
+			}
+		};
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new RicasFlixController(service)).build();
+
+		mockMvc.perform(post("/api/movies/search")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"query\":\"action\"}"))
+				.andExpect(status().isOk());
+	}
+}
